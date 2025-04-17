1c49f88ae0532ad1b47ddbe460a69ba132094e59
Cole Yadron <cole.yadron@gmail.com>
2025-04-17 15:35:54 -0500

    Add basic components to app. Plus theme toggoling

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Alert from "./components/Alert";
+import { useState } from "react";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const items = [
+    'agentOne', 'agentTwo', 'agentThree', 'agentFour', 'agentFive'
+  ];
+  const [alertVisible, setAlertVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
+  const [theme, setTheme] = useState<'light' | 'dark'>('dark');
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  const handleSelectItem = (item: string) => {
+      console.log(item);
+      setSelectedItem(item);
+    }
+  
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  }
+
+  const themeStyle = {
+    backgroundColor: theme === 'light' ? '#f5f5f5' : '#333333',
+    color: theme === 'light' ? '#000' : '#fff',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    minHeight: '100vh',
+  };
+
+  return (<div style={themeStyle}>
+    <div style={{display: 'flex', flexDirection: 'column', width: '300px', gap: '16px', paddingLeft: '16px', paddingRight:'16px', backgroundColor: '#3a3a3a'}}>
+      {alertVisible && <Alert onClose={() => setAlertVisible(false)}>Agent Selected: {selectedItem}</Alert>}
+      <ListGroup items={items} heading="Agents" onSelectItem={handleSelectItem} />
+      <Button color='info'onClick={() => setAlertVisible(true)}>
+        Confirm
+      </Button>
+    </div>
+    <div style={{position: 'absolute', bottom: '16px', left: '16px'}}>
+        <Button color='secondary' onClick={toggleTheme}>
+          Toggle Theme
+        </Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  </div>)
 }
 
-export default App
+export default App;
