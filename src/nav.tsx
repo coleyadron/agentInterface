bb9c7b5b47e2714f7514a44ffdea64b6d6abefe5
Cole Yadron <cole.yadron@gmail.com>
2025-04-17 15:34:53 -0500

    test component

---
+++ src/nav.tsx
@@ -0,0 +1,12 @@
+
+function Message() {
+    const name = "Friend"
+    return (
+        <div>
+        <h1>Hello, {name}!</h1>
+        <p>This is a simple message component.</p>
+        </div>
+    );
+}
+
+export default Message;
